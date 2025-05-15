07cbd25c5f64fb960fec51f82cba415c4e6bcaea
Dedan Opiyo <dedan.opiyo@student.moringaschool.com>
2025-05-15 13:07:36 +0300

    defines classes that inherit from a shared class. Uses super keyword to inherit from and augment methods in the parent class. Child classes have their own unique methods.

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,31 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
+
+
+    # def validRandom(self): # import random
+    #     while True:
+    #         random_no = random() * 10 
+    #         if 0 <= random_no <=7:
+    #             return random_no
 
     def teach(self):
-        pass
+        list_index = random.randint(0, len(self.knowledge)-1)
+        return self.knowledge[list_index]
+    
+# Test
+john = Teacher("John", "Johhanah")
+# print(john.first_name)
+print(john.teach())
